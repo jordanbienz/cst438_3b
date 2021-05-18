bdad9cce30ff3525bf63292defb04816532f3d2e
jordanbienz <jordanbienz@gmail.com>
2021-05-18 15:24:26 -0700

    Update

---
+++ src/test/java/cst4383b/Cst4383bApplicationTests.java
@@ -0,0 +1,13 @@
+package cst4383b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst4383bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
